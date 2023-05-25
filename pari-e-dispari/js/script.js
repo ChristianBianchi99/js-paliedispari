5a38b1eb8ded6d1e25a5cbf7c308fa45ad198fd5
ChristianBianchi99 <bianchi848@gmail.com>
2023-05-25 17:26:13 +0200

    Agg logica js - Es concluso

---
+++ pari-e-dispari/js/script.js
@@ -0,0 +1,36 @@
+// CREAZIONE PROMPT
+let userChoice = (prompt("Pari o Dispari?").trim()).toLocaleLowerCase();
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5").trim());
+
+// CONTROLLI PROMPT
+while(userChoice !== "pari" && userChoice !== "dispari"){
+    userChoice = (prompt("Pari o Dispari?").trim()).toLocaleLowerCase();
+}
+while(isNaN(userNumber) || userNumber > 5 || userNumber < 1){
+    userNumber = parseInt(prompt("Inserisci un numero da 1 a 5").trim());
+}
+
+// GENERAZIONE FUNCTION
+function  rundomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function oddOrEven(a, b){
+    let sum = a + b;
+    if(sum & 2 === 0){
+        return "pari";
+    }
+    return "dispari";
+}
+
+// UTILIZZO FUNCTION
+let compNumber = rundomNumber(1, 5);
+console.log(compNumber)
+let result = oddOrEven(userNumber, compNumber)
+console.log(result)
+
+// DICHIARAZINE VINCITORE
+if(result === userChoice){
+    console.log("Hai vinto")
+} else {
+    console.log("Hai perso")
+}
